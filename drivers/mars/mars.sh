27e2caa3998fb7f51826486971cc01f026a75f41
Camilo Vargas Cabrera <vargax@gmail.com>
2015-11-09 14:02:27 +0100

    mars driver update to new database structure

--- drivers/mars/mars.sh
+++ drivers/mars/mars.sh
@@ -4,11 +4,11 @@ cd ../../data/ignore/mars
 unzip Shape.zip -d /tmp/mars
 
 python ../../../drivers/mars/csv_parser.py
-mv output/* /tmp/mars/.
-rmdir output
+mv mars.sql /tmp/mars/.
 
 cd /tmp/mars
-psql -U mars -d mars -f db.sql
+psql -U mars -d mars -f mars.sql
+
 shp2pgsql -d -e -W 'latin1' MARS-Bogota.shp > shape.sql
 sed -i "s/mars-bogota/mars_bogota/g" shape.sql
 sed -i "s/PRIMARY KEY (gid);/PRIMARY KEY (mars);/g" shape.sql
